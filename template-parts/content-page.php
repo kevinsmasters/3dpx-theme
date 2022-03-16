c4771dec4bc6ae15ddada7f7b0bd3e708321d1e1
kevinsmasters <gmail@kevinmasters.com>
2022-03-15 21:30:10 -0400

    every page hero tweak

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,6 +10,34 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<?php
+if ( !is_front_page() ) : ?>
+<div class="DJ__BannerTile__processed banner-tile tile color-black analytics-container" data-title="Homepage Banner - Hope Changes Lives">
+   <div class="banner-tile__bg">
+	   <picture>
+		   <source media="(min-width: 1920px)" srcset="">
+		   <source media="(min-width: 1281px) and (max-width: 1920px)" 
+		   <?php if(has_header_image()) { ?>srcset="<?php header_image(); ?>" <?php } else { ?>srcset="https://placeimg.com/1440/710/any" <?php } ?>>
+		   <source media="(min-width: 415px) and (max-width: 1280px)" srcset="">
+		   <source media="(max-width: 414px)" srcset="">
+		   <img <?php if(has_header_image()) { ?>src="<?php header_image(); ?>" <?php } else { ?>
+			src="https://placeimg.com/1440/710/any" <?php } ?>
+			width="100%" height="auto" alt="Hope Changes Lives">
+		</picture>
+	</div>
+	<div class="grid-container">
+		<div class="grid-x grid-padding-x">
+			<div class="cell medium-8 medium-offset-2">
+				<div class="banner-tile__content">
+					<?php the_title( '<h1 class="banner-tile__headline">', '</h1>' ); ?>
+					<div class="banner-tile__desc"><?php echo get_post_meta($post->ID, 'hero_text', true); ?></div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<?php endif; ?>
+
 	<?php
     $enable_vc = get_post_meta(get_the_ID(), '_wpb_vc_js_status', true);
     if(!$enable_vc ) {

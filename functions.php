55ce53f2161f7f6a32dd3ba0b373210dcda28d99
kevinsmasters <gmail@kevinmasters.com>
2022-05-26 21:22:49 -0400

    ants and my css changes

--- functions.php
+++ functions.php
@@ -118,7 +118,7 @@ add_action( 'widgets_init', 'threedpx_bootstrap_widgets_init' );
 // Replaces the excerpt "Read More" text by a link
 function new_excerpt_more($more) {
     global $post;
- return '<a class="moretag" href="'. get_permalink($post->ID) . '"> <br/>Read the full article...</a>';
+ return '<a class="moretag" href="'. get_permalink($post->ID) . '"> continue reading...</a>';
 }
 add_filter('excerpt_more', 'new_excerpt_more');
 
